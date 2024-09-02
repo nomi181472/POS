1a612beac657084d50e28182cbdaa0a6d8e6dd9f
MaaribUlHaqSiddiqui <maarib12345@gmail.com>
2024-09-02 11:17:22 +0500

    UserActivityLogging implemented using redis

--- Host/Hub/Program.cs
+++ Host/Hub/Program.cs
@@ -8,6 +8,7 @@
 
 //app.Run();
 using Hub.Extensions;
+using UserActivity;
 
 try
 {
@@ -15,6 +16,19 @@ try
     builder.Services.RegisterService(builder.Configuration);
     var app = builder.Build();
     await app.Configure();
+
+    /*app.MapGet("/api/user-activity/{userId}", async (string userId, IUserActivity userActivity) =>
+    {
+        var logs = await userActivity.GetActivityAsync(userId);
+        return logs ?? "No logs found for this user.";
+    });*/
+
+    app.MapGet("/api/user-activity2/{userId}", async (string userId, IUserActivity userActivity) =>
+    {
+        var logs = await userActivity.GetActivityAsync(userId);
+        return logs.Any() ? Results.Ok(logs) : Results.NotFound("No logs found for this user.");
+    });
+
     app.Run();
 }
 catch (Exception ex)
---
+++ UserActivity/UserActivity.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserActivity
+{
+    public class UserActivity: IUserActivity
+    {
+        private readonly ConnectionMultiplexer _multiplexer;
+        readonly IDatabase _database;
+
+        public UserActivity(ConnectionMultiplexer multiplexer)
+        {
+            _multiplexer = multiplexer;
+            _database = multiplexer.GetDatabase();
+
+        }
+
+        public async Task LogActivityAsync(string userId, string activity)
+        {
+            var key = $"activity_{userId}";
+            //bool result=await _database.StringSetAsync (key, activity,expiry:TimeSpan.FromSeconds(100));
+            await _database.ListRightPushAsync(key, activity);
+
+        }
+
+        public async Task<IEnumerable<string>> GetActivityAsync(string userId)
+        {
+            var key = $"activity_{userId}";
+            //return await _database.StringGetAsync(key);
+
+            var activities = await _database.ListRangeAsync(key);
+            return activities.Select(a => a.ToString());
+        }
+    }
+}
