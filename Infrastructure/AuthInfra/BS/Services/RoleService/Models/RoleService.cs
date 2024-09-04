e625f409beefb3cd6442e64cbe7190176c2cfd50
Noman Ali <33200799+nomi181472@users.noreply.github.com>
2024-09-04 22:30:29 +0500

    roles services added

---
+++ Infrastructure/AuthInfra/BS/Services/RoleService/Models/RoleService.cs
@@ -0,0 +1,38 @@
+﻿using BS.Services.RoleService.Models.Request;
+using BS.Services.RoleService.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS.Services.RoleService.Models
+{
+    public class RoleService : IRoleService
+    {
+        public Task<bool> AddRoleToUser(RequestAddRoleToUser request, string userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DetachUserRole(string roleId, string userId, CancellationToken CancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DetachUserRoles(string[] roleId, string userId, CancellationToken CancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResponseGetAllUserRoles>> GetAllUserRoles(string userId, CancellationToken CancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseGetAllUserRoles> GetUserRoleById(string roleId, CancellationToken CancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
