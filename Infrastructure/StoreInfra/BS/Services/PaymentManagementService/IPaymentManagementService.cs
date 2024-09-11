897eb0bc5fc4727ea6d291bb37e6598fe1b239af
Usaid <pc@DESKTOP-PTRUD04>
2024-09-10 23:02:13 -0700

    feature - Add SurchargeAndDiscount OF PaymentManagementService OF Store

---
+++ Infrastructure/StoreInfra/BS/Services/PaymentManagementService/IPaymentManagementService.cs
@@ -0,0 +1,15 @@
+﻿using BS.Services.PaymentManagementService.Models.Request;
+using BS.Services.PaymentManagementService.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS.Services.PaymentManagementService
+{
+    public interface IPaymentManagementService
+    {
+        Task<ResponseAddSurchargeDiscount> AddSurchargeDiscount(RequestAddSurchargeDiscount request, string userId, CancellationToken token);
+    }
+}
---
+++ Infrastructure/StoreInfra/BS/Services/PaymentManagementService/Models/Request/RequestAddSurchargeDiscount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS.Services.PaymentManagementService.Models.Request
+{
+    public class RequestAddSurchargeDiscount
+    {
+        public string PaymentType { get; set; }
+
+        public double TotalBill { get; set; }
+
+        public double CashAmount { get; set; }
+
+        public double CardAmount { get; set; }
+    }
+}
