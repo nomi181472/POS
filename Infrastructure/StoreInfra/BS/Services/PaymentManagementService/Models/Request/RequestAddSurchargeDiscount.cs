028bab0afb45a078c143e89d97a17b7f954226e6
Usaid <pc@DESKTOP-PTRUD04>
2024-09-11 23:36:09 -0700

    refactor - Add SurchargeDiscount OF Payment TO cater req 7.2 & 7.4 overlap implement naive SplitPayments

--- Infrastructure/StoreInfra/BS/Services/PaymentManagementService/Models/Request/RequestAddSurchargeDiscount.cs
+++ Infrastructure/StoreInfra/BS/Services/PaymentManagementService/Models/Request/RequestAddSurchargeDiscount.cs
@@ -8,12 +8,16 @@ namespace BS.Services.PaymentManagementService.Models.Request
 {
     public class RequestAddSurchargeDiscount
     {
-        public string PaymentType { get; set; }
+        //public string PaymentType { get; set; }
 
         public double TotalBill { get; set; }
 
+        public string DiscountVoucherCode { get; set; }
+        public int LoyaltyPointsRedeemed { get; set; }
+        public double ChequeAmount { get; set; }
+        public double OnlineTransferAmount {  get; set; }
         public double CashAmount { get; set; }
-
         public double CardAmount { get; set; }
+        public bool IsCredit { get; set; }
     }
 }
