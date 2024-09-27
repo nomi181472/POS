30a224fe131fa7e72a86b926c34d9f1e0d0ad10d
Usaid <pc@DESKTOP-PTRUD04>
2024-09-27 00:47:41 -0700

    FEATURE - redisSessionManagement LOGIN only

---
+++ SessionManager/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionManager
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRedisSessionManagement(this IServiceCollection services, IConfiguration configuration)
+        {
+            var redisConnectionString = configuration.GetConnectionString("Redis");
+
+            services.AddSingleton<IConnectionMultiplexer>(sp =>
+            {
+                var configurationOptions = ConfigurationOptions.Parse(redisConnectionString, true);
+                return ConnectionMultiplexer.Connect(configurationOptions);
+            });
+
+            services.AddScoped<RedisSessionManager>();
+
+            return services;
+        }
+    }
+}
