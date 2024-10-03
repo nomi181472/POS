66329e9fa23aff606913a8fc35da707c6af328f6
Usaid <pc@DESKTOP-PTRUD04>
2024-10-03 00:53:35 -0700

    HOTFIX - ApiConfigs for Server AddActionsInRole

--- Infrastructure/AuthInfra/BS/ExternalServices/ApiConfigs.cs
+++ Infrastructure/AuthInfra/BS/ExternalServices/ApiConfigs.cs
@@ -8,14 +8,14 @@ namespace BS.ExternalServices
         {
             public static string Name { get; set; } = "Store";
             public static int Port { get; set; } = 6082;
-            public static string Host { get; set; } = "localhost";
+            public static string Host { get; set; } = "store";
 
         }
         public static class HubApiConfig
         {
             public static string Name { get; set; } = "Hub";
             public static int Port { get; set; } = 9082;
-            public static string Host { get; set; } = "localhost";
+            public static string Host { get; set; } = "hub";
 
         }
 
